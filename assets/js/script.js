43585404b5eac9d43cd8267201e2128cca4553dc
jbrowning824 <31068426+jbrowning824@users.noreply.github.com>
2023-10-02 15:58:41 -0400

    added starting js structure

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var lon = 0;
+var lat = 0;
+var city = "";
+var stateCode = "";
+var countryCode = "";
+var apikey = "6e2a33cd6f27746a05153a52516eb6b3";
+var weatherApiUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apikey}`;
+var geocodingApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},US&appid=${apikey}`;
+
+
+function getStateGeocooridnates() {
+    
+}
+
+function getWeatherForecast() {
+    
+}
+
+function createWeatherCards() {
+    
+}
