8c76597ddc8b48c30f2cf83b6c6d7d34a67b5ecb
jbrowning824 <31068426+jbrowning824@users.noreply.github.com>
2023-10-03 12:02:16 -0400

    created fetch for city and 5 day weather, verified functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,30 @@
 var lon = 0;
 var lat = 0;
-var city = "";
 var stateCode = "";
 var countryCode = "";
 var apikey = "6e2a33cd6f27746a05153a52516eb6b3";
-var weatherApiUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apikey}`;
-var geocodingApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},US&appid=${apikey}`;
+var weatherApiUrl = `http://api.openweathermap.org/data/2.5/forecast?appid=${apikey}`;
+var geocodingApiUrl = `http://api.openweathermap.org/geo/1.0/direct?appid=${apikey}`;
 
+$("form").submit(function(){
+        event.preventDefault();
+        var result = $('#search').val()
+        $('#search').val('');
+        getCityGeocooridnates(result);
+      });
 
-function getStateGeocooridnates() {
-    
+async function getCityGeocooridnates(city) {
+    const response = await fetch(geocodingApiUrl + `&q=${city},US`);
+    getWeatherForecast(await response.json());
 }
 
-function getWeatherForecast() {
-    
+async function getWeatherForecast(coordinateResponse) {
+
+    const response = await fetch(weatherApiUrl + `&lat=${coordinateResponse[0].lat}&lon=${coordinateResponse[0].lon}`);
+    createWeatherCards(await response.json());
+
 }
 
-function createWeatherCards() {
-    
+function createWeatherCards(forecast) {
+    console.log(forecast);
 }
